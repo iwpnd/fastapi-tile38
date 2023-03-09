897c082d7411868e1a69ef4d58f938546439b1a8
Benjamin Ramser <legionaerr@googlemail.com>
2023-03-09 16:23:29 +0100

    fix: settings

--- src/app/config/settings.py
+++ src/app/config/settings.py
@@ -1,12 +1,14 @@
+from typing import Optional
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    TILE38_URI: str
+    TILE38_URI: Optional[str] = None
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
 
 
-settings = Settings(_env_file=".env", _env_file_encoding="utf-8")
+settings = Settings()
